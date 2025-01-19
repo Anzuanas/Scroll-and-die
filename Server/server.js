20e8ef8b08bf7e695d173d8e48ae67d307aab134
anas <anasginolt@gmail.com>
2025-01-19 19:23:05 +0530

    json error

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/connectDB.js"
@@ -17,6 +18,7 @@ cloudinary.config({
 
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({extended: true}))
@@ -26,5 +28,14 @@ app.use(cookieParser())
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/messages",messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 server.listen (port, () => console.log(`server is running on port ${port}`))
