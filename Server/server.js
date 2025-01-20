aecedb9b7a0e560096bdeb2dcf72a40386e4e4d2
anas <anasginolt@gmail.com>
2025-01-20 14:01:21 +0530

    deployment

--- Server/server.js
+++ Server/server.js
@@ -30,11 +30,11 @@ app.use("/api/posts",postRoutes)
 app.use("/api/messages",messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Client/dist")));
 
 	// react app
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Client", "dist", "index.html"));
 	});
 }
 
